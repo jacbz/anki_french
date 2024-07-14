d1f7d72e590dd86118f5cc5bd3baa3976392f20c
Jacob Zhang <jacob-z@live.de>
2024-07-14 17:41:21 +0200

    Add versioning to fix sync media issues

--- card_templates/back.js
+++ card_templates/back.js
@@ -283,7 +283,7 @@ let grammar = {
   content: {},
   github: {},
 };
-fetch(`${getAnkiPrefix()}/_FR5000_grammar.json`)
+fetch(`${getAnkiPrefix()}/_FR5000_grammar____VERSION___.json`)
   .then((response) => response.json())
   .then((loadedGrammar) => {
     grammar = loadedGrammar;
@@ -579,7 +579,7 @@ if (rank >= 1 && rank <= 5000) {
 /**
  * Update notice
  */
-const lastUpdated = new Date("___LAST_UPDATED___");
+const lastUpdated = new Date("___VERSION___");
 const numberOfDaysSince = Math.floor(
   (Date.now() - lastUpdated.getTime()) / (1000 * 60 * 60 * 24)
 );
